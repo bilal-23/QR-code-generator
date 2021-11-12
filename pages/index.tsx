9b57f1cf6f086662fc9893d11ca900ffcb538132
Bilal <mansuribilal101@gmail.com>
2021-11-12 22:28:19 +0530

    Initial Commit

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <div className="flex justify-center mt-10">
+      <p className="text-6xl">Hello This is first tailwind project</p>
+    </div>
+  );
+};
+
+export default Home;
